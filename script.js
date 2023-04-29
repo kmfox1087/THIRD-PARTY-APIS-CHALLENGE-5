8c2968e4e4d5b50cfcbb855e89906848486f9098
kmfox1087 <kmfox1087@gmail.com>
2023-04-29 09:40:59 -0400

    simplified html to make it easier for me to read, set variables in js and assigned ids for writingblocks

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
+//Global variables
+var $time = $('currentDay');
+var getTime = moment().hour();
+// 9am button
+var button9 = document.getElementById('btn9');
+// 10am button
+var button10 = document.getElementById('btn10');
+// 11 am button
+var button11 = document.getElementById('btn11');
+// 12 pm button
+var button12 = document.getElementById('btn12');
+// 1pm button
+var button1 = document.getElementById('btn1');
+// 2pm button
+var button2 = document.getElementById('btn2');
+// 3pm button
+var button3 = document.getElementById('btn3');
+// 4pm button
+var button4 = document.getElementById('btn4');
+// 5 pm button
+var button5 = document.getElementById('btn5');
+// Clear button
+var ClearBtn = document.getElementById('clear');
+
+// Assigning 'writingblock' to ID's 
+let writingblock = document.querySelector('#NINEAM');
+let writingblock2 = document.querySelector('#TENAM');
+let writingblock3 = document.querySelector('#ELEVEN');
+let writingblock4 = document.querySelector('#TWELVE');
+let writingblock5 = document.querySelector('#ONE');
+let writingblock6 = document.querySelector('#TWO');
+let writingblock7 = document.querySelector('#THREE');
+let writingblock8 = document.querySelector('#FOUR');
+let writingblock9 = document.querySelector('#FIVE');
+
   // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  
+
   // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  
   // TODO: Add code to display the current date in the header of the page.
-});
