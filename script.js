a4876c161ad6aea2f1af8593b2ea78d4af9ab078
kmfox1087 <kmfox1087@gmail.com>
2023-04-30 09:02:54 -0400

    fixed bugs in html and js, adjusting some css styling for the writing blocks

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
+// Global Variables
 var $time = $('#currentDay');
 var getTime = moment().hour();
-var button9 = document.getElementById('btn1');
+
+var button9 = document.getElementById('btn9');
 var button10 = document.getElementById('btn10');
-var button11 = document.getElementById('btn3');
+var button11 = document.getElementById('btn11');
 var button12 = document.getElementById('btn12');
-var button1 = document.getElementById('btn11');
-var button2 = document.getElementById('btn22');
-var button3 = document.getElementById('btn33');
+var button1 = document.getElementById('btn1');
+var button2 = document.getElementById('btn2');
+var button3 = document.getElementById('btn3');
 var button4 = document.getElementById('btn4');
 var button5 = document.getElementById('btn5');
 var ClearBtn = document.getElementById('clear');
 
-//assign scratchpad to id's
-let scratchpad = document.querySelector('#NINEAM');
-let scratchpad2 = document.querySelector('#TENAM');
-let scratchpad3 = document.querySelector('#ELEVEN');
-let scratchpad4 = document.querySelector('#TWELVE');
-let scratchpad5 = document.querySelector('#ONE');
-let scratchpad6 = document.querySelector('#TWO');
-let scratchpad7 = document.querySelector('#THREE');
-let scratchpad8 = document.querySelector('#FOUR');
-let scratchpad9 = document.querySelector('#FIVE');
+//assign writingblock to id's
+let writingblock = document.querySelector('#NINEAM');
+let writingblock2 = document.querySelector('#TENAM');
+let writingblock3 = document.querySelector('#ELEVEN');
+let writingblock4 = document.querySelector('#TWELVE');
+let writingblock5 = document.querySelector('#ONE');
+let writingblock6 = document.querySelector('#TWO');
+let writingblock7 = document.querySelector('#THREE');
+let writingblock8 = document.querySelector('#FOUR');
+let writingblock9 = document.querySelector('#FIVE');
 
 //Check to clear data if needed
 if (getTime == 0) {
     localStorage.clear();
 }
 //Gets localstorage
-scratchpad.value = localStorage.getItem('Nine');
-scratchpad2.value = localStorage.getItem('Ten');
-scratchpad3.value = localStorage.getItem('Eleven');
-scratchpad4.value = localStorage.getItem('Twelve');
-scratchpad5.value = localStorage.getItem('One');
-scratchpad6.value = localStorage.getItem('Two');
-scratchpad7.value = localStorage.getItem('Three');
-scratchpad8.value = localStorage.getItem('Four');
-scratchpad9.value = localStorage.getItem('Five');
+writingblock.value = localStorage.getItem('Nine');
+writingblock2.value = localStorage.getItem('Ten');
+writingblock3.value = localStorage.getItem('Eleven');
+writingblock4.value = localStorage.getItem('Twelve');
+writingblock5.value = localStorage.getItem('One');
+writingblock6.value = localStorage.getItem('Two');
+writingblock7.value = localStorage.getItem('Three');
+writingblock8.value = localStorage.getItem('Four');
+writingblock9.value = localStorage.getItem('Five');
 
 //Display current time
 window.setInterval(function () {
@@ -44,32 +46,32 @@ window.setInterval(function () {
 
 //Function to save notes
 function SaveNotes() {
-    localStorage.setItem('Nine', scratchpad.value)
+    localStorage.setItem('Nine', writingblock.value)
 }
 
 function SaveNotes2() {
-    localStorage.setItem('Ten', scratchpad2.value)
+    localStorage.setItem('Ten', writingblock2.value)
 }
 function SaveNotes3() {
-    localStorage.setItem('Eleven', scratchpad3.value)
+    localStorage.setItem('Eleven', writingblock3.value)
 }
 function SaveNotes4() {
-    localStorage.setItem('Twelve', scratchpad4.value)
+    localStorage.setItem('Twelve', writingblock4.value)
 }
 function SaveNotes5() {
-    localStorage.setItem('One', scratchpad5.value)
+    localStorage.setItem('One', writingblock5.value)
 }
 function SaveNotes6() {
-    localStorage.setItem('Two', scratchpad6.value)
+    localStorage.setItem('Two', writingblock6.value)
 }
 function SaveNotes7() {
-    localStorage.setItem('Three', scratchpad7.value)
+    localStorage.setItem('Three', writingblock7.value)
 }
 function SaveNotes8() {
-    localStorage.setItem('Four', scratchpad8.value)
+    localStorage.setItem('Four', writingblock8.value)
 }
 function SaveNotes9() {
-    localStorage.setItem('Five', scratchpad9.value)
+    localStorage.setItem('Five', writingblock9.value)
 }
 
 //Click events for the save buttons
@@ -91,7 +93,7 @@ ClearBtn.addEventListener('click', function() {
 
 // If statements to change block colors 
 if (getTime == 9) {
-    document.getElementById('Nineam').style.backgroundColor = "white";
+    document.getElementById('Nineam').style.backgroundColor = "White";
 } else if (getTime < 9) {
     document.getElementById('Nineam').style.backgroundColor = "Green";
 } else {
@@ -99,7 +101,7 @@ if (getTime == 9) {
 }
 
 if (getTime == 10) {
-    document.getElementById('Tenam').style.backgroundColor = "white";
+    document.getElementById('Tenam').style.backgroundColor = "White";
 } else if (getTime < 10) {
     document.getElementById('Tenam').style.backgroundColor = "Green";
 } else {
@@ -107,7 +109,7 @@ if (getTime == 10) {
 }
 
 if (getTime == 11) {
-    document.getElementById('Elevenam').style.backgroundColor = "white";
+    document.getElementById('Elevenam').style.backgroundColor = "White";
 } else if (getTime < 11) {
     document.getElementById('Elevenam').style.backgroundColor = "Green";
 } else {
@@ -115,7 +117,7 @@ if (getTime == 11) {
 }
 
  if(getTime==12){
-     document.getElementById('Twelevepm').style.backgroundColor = "white";
+     document.getElementById('Twelevepm').style.backgroundColor = "White";
  }else if(getTime<12){
     document.getElementById('Twelevepm').style.backgroundColor = "Green";
  }else {
@@ -123,7 +125,7 @@ if (getTime == 11) {
  }
 
 if(getTime==13){
-   document.getElementById('Onepm').style.backgroundColor = "white";
+   document.getElementById('Onepm').style.backgroundColor = "White";
  }else if(getTime<13){
     document.getElementById('Onepm').style.backgroundColor = "Green";
 }else {
@@ -131,7 +133,7 @@ if(getTime==13){
  }
 
 if(getTime==14){
-    document.getElementById('Twopm').style.backgroundColor = "white";
+    document.getElementById('Twopm').style.backgroundColor = "White";
  }else if(getTime<14){
      document.getElementById('Twopm').style.backgroundColor = "Green";
  }else {
@@ -139,7 +141,7 @@ if(getTime==14){
  }
 
 if(getTime==15){
-    document.getElementById('Threepm').style.backgroundColor = "white";
+    document.getElementById('Threepm').style.backgroundColor = "White";
 }else if(getTime<15){
     document.getElementById('Threepm').style.backgroundColor = "Green";
  }else {
@@ -147,7 +149,7 @@ if(getTime==15){
 }
 
  if(getTime==16){
-    document.getElementById('Fourpm').style.backgroundColor = "white";
+    document.getElementById('Fourpm').style.backgroundColor = "White";
  }else if(getTime<16){
     document.getElementById('Fourpm').style.backgroundColor = "Green";
  }else {
@@ -155,7 +157,7 @@ if(getTime==15){
  }
 
  if(getTime==17){
-    document.getElementById('Fivepm').style.backgroundColor = "white";
+    document.getElementById('Fivepm').style.backgroundColor = "White";
  }else if(getTime<17){
      document.getElementById('Fivepm').style.backgroundColor = "Green";
  }else {
