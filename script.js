19d380e70be3e8d33ce74e71cb7d473fb7ee9c1b
kmfox1087 <kmfox1087@gmail.com>
2023-04-30 08:40:49 -0400

    changed bugs in code, added css styling

--- script.js
+++ script.js
@@ -1,170 +1,163 @@
-//Global variables
-var $time = $('currentDay');
+var $time = $('#currentDay');
 var getTime = moment().hour();
-// 9am button
-var button9 = document.getElementById('btn9');
-// 10am button
+var button9 = document.getElementById('btn1');
 var button10 = document.getElementById('btn10');
-// 11 am button
-var button11 = document.getElementById('btn11');
-// 12 pm button
+var button11 = document.getElementById('btn3');
 var button12 = document.getElementById('btn12');
-// 1pm button
-var button1 = document.getElementById('btn1');
-// 2pm button
-var button2 = document.getElementById('btn2');
-// 3pm button
-var button3 = document.getElementById('btn3');
-// 4pm button
+var button1 = document.getElementById('btn11');
+var button2 = document.getElementById('btn22');
+var button3 = document.getElementById('btn33');
 var button4 = document.getElementById('btn4');
-// 5 pm button
 var button5 = document.getElementById('btn5');
-// Clear button
 var ClearBtn = document.getElementById('clear');
 
-// Assigning 'writingblock' to ID's 
-let writingblock = document.querySelector('#NINEAM');
-let writingblock2 = document.querySelector('#TENAM');
-let writingblock3 = document.querySelector('#ELEVEN');
-let writingblock4 = document.querySelector('#TWELVE');
-let writingblock5 = document.querySelector('#ONE');
-let writingblock6 = document.querySelector('#TWO');
-let writingblock7 = document.querySelector('#THREE');
-let writingblock8 = document.querySelector('#FOUR');
-let writingblock9 = document.querySelector('#FIVE');
+//assign scratchpad to id's
+let scratchpad = document.querySelector('#NINEAM');
+let scratchpad2 = document.querySelector('#TENAM');
+let scratchpad3 = document.querySelector('#ELEVEN');
+let scratchpad4 = document.querySelector('#TWELVE');
+let scratchpad5 = document.querySelector('#ONE');
+let scratchpad6 = document.querySelector('#TWO');
+let scratchpad7 = document.querySelector('#THREE');
+let scratchpad8 = document.querySelector('#FOUR');
+let scratchpad9 = document.querySelector('#FIVE');
 
-  // TODO: Add a listener for click events on the save button
-
-  button9.addEventListener('click', SaveNotes);
-  button10.addEventListener('click', SaveNotes);
-  button11.addEventListener('click', SaveNotes);
-  button12.addEventListener('click', SaveNotes);
-  button1.addEventListener('click', SaveNotes);
-  button2.addEventListener('click', SaveNotes);
-  button3.addEventListener('click', SaveNotes);
-  button4.addEventListener('click', SaveNotes);
-  button5.addEventListener('click', SaveNotes);
-
-  // Event listener for clear button
-
-  ClearBtn.addEventListener('click', function () {
+//Check to clear data if needed
+if (getTime == 0) {
     localStorage.clear();
-    location.reload();
-  })
-
-  // If statements to change background color depending on time of day
-
-  if (getTime==9) {
-    document.getElementById('Nineam').style.backgroundColor = "white";
-  } else if (getTime < 9) {
-    document.getElementById('Nineam').style.backgroundColor = "green";
-  } else {
-    document.getElementById('Nineam').style.backgroundColor = "red";
-  }
-
-  if (getTime==10) {
-    document.getElementById('Tenam').style.backgroundColor = "white";
-  } else if (getTime < 10) {
-    document.getElementById('Tenam').style.backgroundColor = "green";
-  } else {
-    document.getElementById('Tenam').style.backgroundColor = "red";
-  }
-
-  if (getTime == 11) {
-    document.getElementById('Eleveam').style.backgroundColor = "white";
-  } else if (getTime < 11) {
-    document.getElementById('Elevenam').style.backgroundColor = "green";
-  } else {
-    document.getElementById('Elevenam').style.backgroundColor = "red";
-  }
-
-  if (getTime == 12) {
-    document.getElementById('Twelvepm').style.backgroundColor = "white";
-  } else if (getTime < 12) {
-    document.getElementById('Twelvepm').style.backgroundColor = "green";
-  } else {
-    document.getElementById('Twelvepm').style.backgroundColor = "red";
-  }
-
-  if (getTime == 13) {
-    document.getElementById('Onepm').style.backgroundColor = "white";
-  } else if (getTime < 13) {
-    document.getElementById('Onepm').style.backgroundColor = "green";
-  } else {
-    document.getElementById('Onepm').style.backgroundColor = "red";
-  }
-
-  if (getTime == 14) {
-    document.getElementById('Twopm').style.backgroundColor = "white";
-  } else if (getTime < 14) {
-    document.getElementById('Twopm').style.backgroundColor = "green";
-  } else {
-    document.getElementById('Twopm').style.backgroundColor = "red";
-  }
-
-  if (getTime == 15) {
-    document.getElementById('Threepm').style.backgroundColor = "white";
-  } else if (getTime < 15) {
-    document.getElementById('Threepm').style.backgroundColor = "green";
-  } else {
-    document.getElementById('Threepm').style.backgroundColor = "red";
-  }
-
-  if (getTime==16) {
-    document.getElementById('Fourpm').style.backgroundColor = "white";
-  } else if (getTime < 16) {
-    document.getElementById('Fourpm').style.backgroundColor = "green";
-  } else {
-    document.getElementById('Fourpm').style.backgroundColor = "red";
-  }
-
-  if (getTime==17) {
-    document.getElementById('Fivepm').style.backgroundColor = "white";
-  } else if (getTime < 17) {
-    document.getElementById('Fivepm').style.backgroundColor = "green";
-  } else {
-    document.getElementById('Fivepm').style.backgroundColor = "red";
-  }
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // Function to save the user's notes
-function SaveNotes () {
-  localStorage.setItem('Nine', writingblock.value)
 }
+//Gets localstorage
+scratchpad.value = localStorage.getItem('Nine');
+scratchpad2.value = localStorage.getItem('Ten');
+scratchpad3.value = localStorage.getItem('Eleven');
+scratchpad4.value = localStorage.getItem('Twelve');
+scratchpad5.value = localStorage.getItem('One');
+scratchpad6.value = localStorage.getItem('Two');
+scratchpad7.value = localStorage.getItem('Three');
+scratchpad8.value = localStorage.getItem('Four');
+scratchpad9.value = localStorage.getItem('Five');
 
-function SaveNotes () {
-  localStorage.setItem('Ten', writingblock2.value)
-}
-
-function SaveNotes () {
-  localStorage.setItem('Eleven', writingblock3.value)
-}
-
-function SaveNotes () {
-  localStorage.setItem('Twelve', writingblock4.value)
-}
-
-function SaveNotes () {
-  localStorage.setItem('One', writingblock5.value)
-}
-
-function SaveNotes () {
-  localStorage.setItem('Two', writingblock6.value)
-}
-
-function SaveNotes () {
-  localStorage.setItem('Three', writingblock7.value)
-}
-
-function SaveNotes () {
-  localStorage.setItem('Four', writingblock8.value)
-}
-
-function SaveNotes () {
-  localStorage.setItem('Five', writingblock9.value)
-}
-  // TODO: Add code to display the current date in the header of the page.
-
-  window.setInterval(function () {
+//Display current time
+window.setInterval(function () {
     $('#currentDay').html(moment().format('MMMM Do YYYY, h:mm:ss a'))
 }, 1000);
+
+//Function to save notes
+function SaveNotes() {
+    localStorage.setItem('Nine', scratchpad.value)
+}
+
+function SaveNotes2() {
+    localStorage.setItem('Ten', scratchpad2.value)
+}
+function SaveNotes3() {
+    localStorage.setItem('Eleven', scratchpad3.value)
+}
+function SaveNotes4() {
+    localStorage.setItem('Twelve', scratchpad4.value)
+}
+function SaveNotes5() {
+    localStorage.setItem('One', scratchpad5.value)
+}
+function SaveNotes6() {
+    localStorage.setItem('Two', scratchpad6.value)
+}
+function SaveNotes7() {
+    localStorage.setItem('Three', scratchpad7.value)
+}
+function SaveNotes8() {
+    localStorage.setItem('Four', scratchpad8.value)
+}
+function SaveNotes9() {
+    localStorage.setItem('Five', scratchpad9.value)
+}
+
+//Click events for the save buttons
+button9.addEventListener('click', SaveNotes)
+button10.addEventListener('click', SaveNotes2)
+button11.addEventListener('click', SaveNotes3)
+button12.addEventListener('click', SaveNotes4)
+button1.addEventListener('click', SaveNotes5)
+button2.addEventListener('click',SaveNotes6)
+button3.addEventListener('click',SaveNotes7)
+button4.addEventListener('click',SaveNotes8)
+button5.addEventListener('click',SaveNotes9)
+
+ClearBtn.addEventListener('click', function() {  
+    localStorage.clear();
+    location.reload();
+
+});
+
+// If statements to change block colors 
+if (getTime == 9) {
+    document.getElementById('Nineam').style.backgroundColor = "white";
+} else if (getTime < 9) {
+    document.getElementById('Nineam').style.backgroundColor = "Green";
+} else {
+    document.getElementById('Nineam').style.backgroundColor = "Red";
+}
+
+if (getTime == 10) {
+    document.getElementById('Tenam').style.backgroundColor = "white";
+} else if (getTime < 10) {
+    document.getElementById('Tenam').style.backgroundColor = "Green";
+} else {
+    document.getElementById('Tenam').style.backgroundColor = "Red";
+}
+
+if (getTime == 11) {
+    document.getElementById('Elevenam').style.backgroundColor = "white";
+} else if (getTime < 11) {
+    document.getElementById('Elevenam').style.backgroundColor = "Green";
+} else {
+    document.getElementById('Elevenam').style.backgroundColor = "Red";
+}
+
+ if(getTime==12){
+     document.getElementById('Twelevepm').style.backgroundColor = "white";
+ }else if(getTime<12){
+    document.getElementById('Twelevepm').style.backgroundColor = "Green";
+ }else {
+         document.getElementById('Twelevepm').style.backgroundColor = "Red";
+ }
+
+if(getTime==13){
+   document.getElementById('Onepm').style.backgroundColor = "white";
+ }else if(getTime<13){
+    document.getElementById('Onepm').style.backgroundColor = "Green";
+}else {
+     document.getElementById('Onepm').style.backgroundColor = "Red";
+ }
+
+if(getTime==14){
+    document.getElementById('Twopm').style.backgroundColor = "white";
+ }else if(getTime<14){
+     document.getElementById('Twopm').style.backgroundColor = "Green";
+ }else {
+    document.getElementById('Twopm').style.backgroundColor = "Red";
+ }
+
+if(getTime==15){
+    document.getElementById('Threepm').style.backgroundColor = "white";
+}else if(getTime<15){
+    document.getElementById('Threepm').style.backgroundColor = "Green";
+ }else {
+     document.getElementById('Threepm').style.backgroundColor = "Red";
+}
+
+ if(getTime==16){
+    document.getElementById('Fourpm').style.backgroundColor = "white";
+ }else if(getTime<16){
+    document.getElementById('Fourpm').style.backgroundColor = "Green";
+ }else {
+    document.getElementById('Fourpm').style.backgroundColor = "Red";
+ }
+
+ if(getTime==17){
+    document.getElementById('Fivepm').style.backgroundColor = "white";
+ }else if(getTime<17){
+     document.getElementById('Fivepm').style.backgroundColor = "Green";
+ }else {
+     document.getElementById('Fivepm').style.backgroundColor = "Red";
+ }
